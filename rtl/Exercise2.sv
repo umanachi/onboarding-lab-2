3e016475a7bff4455daba545de0735eefd45b010
umanachi <123425184+umanachi@users.noreply.github.com>
2023-02-06 19:17:31 -0500

    completed lab 2 exercise 2

--- rtl/Exercise2.sv
+++ rtl/Exercise2.sv
@@ -12,5 +12,20 @@ module Exercise2 (
     input [15:0] init,
     output logic [15:0] out
 );
+  
+  reg [15:0] state;
+  
+  wire taps;
+  
+  assign taps = ((state[15] ^ state[13]) ^ state[12]) ^ state[10];
+  
+  assign out = state;
+  
+  always_ff @ (posedge clk) begin
+    if (~nReset)
+      state <= init;
+    else
+      state <= (state << 1) | {15b'0, taps};
+  end
 
 endmodule
