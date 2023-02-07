5a995260181e31286398ca01b534ed7e163e4a2f
umanachi <123425184+umanachi@users.noreply.github.com>
2023-02-06 19:30:58 -0500

    completed lab 2 exercise 3

--- rtl/Exercise3.sv
+++ rtl/Exercise3.sv
@@ -6,5 +6,9 @@ module Exercise3 (
     input [15:0] c,
     output [15:0] out
 );
+    
+    Mystery1 Alpha(a[1:0], b[7:0], c[7:0], d_alpha);
+    Mystery1 Beta(a[3:2], b[15:8], c[15:8], d_beta);
+    Mystery2 Gamma(clk, nReset, d_alpha, d_beta, out);
 
 endmodule
